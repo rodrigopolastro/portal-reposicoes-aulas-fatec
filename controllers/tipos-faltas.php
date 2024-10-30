2fa6514d8bef5929f20d654e36e5101d11325031
Rodrigo Polastro <rodrigo.polastro03@gmail.com>
2024-10-30 20:35:04 -0300

    feat: modifica estrutura do controller de tipos de faltas para receber parametros via JSON e $_POST

--- controllers/tipos-faltas.php
+++ controllers/tipos-faltas.php
@@ -1,14 +1,22 @@
 <?php
-
-require_once $_SERVER['DOCUMENT_ROOT'] . 
-    '/portal-reposicoes-aulas-fatec/helpers/caminho-absoluto.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/portal-reposicoes-aulas-fatec/helpers/caminho-absoluto.php';
 require_once caminhoAbsoluto('models/tipos-faltas.php');
 
-if (isset($_POST['acao'])) {
-    controllerTiposFaltas($_POST['acao']);
+$jsonRequest = json_decode(file_get_contents('php://input'), true);
+
+if (isset($jsonRequest['acao'])) {
+    $params = $jsonRequest['params'] ?? [];
+    $response = controllerTiposFaltas($jsonRequest['acao'], $params);
+    echo json_encode($response);
+} else if (isset($_POST['acao'])) {
+    $params = $_POST['params'] ?? [];
+    $response = controllerTiposFaltas($_POST['acao'], $params);
+    echo json_encode($response);
+} else {
+    echo json_encode(['erro' => 'Ação não definida.']);
 }
 
-function controllerTiposFaltas($acao)
+function controllerTiposFaltas($acao, $params = [])
 {
     switch ($acao) {
         case 'selectTiposFaltas':
