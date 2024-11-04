b8a7665db23b134d930ecaff6cf69cace20e96cb
Dante <pedrodantecandido@gmail.com>
2024-11-04 15:37:41 -0300

    altera lista-enviados para exibir o pdf e ir para form-reposicao | cria avaliar-falta , avaliar-reposicao e altera lista-recebidos coordenador

---
+++ views/professor/header-professor.php
@@ -0,0 +1,22 @@
+<header>
+    <div class  ="topo">
+        <div class="fundo"><img src="img/logo-governo-do-estado-sp.png" alt="logo" class="logo-governo"></div>
+        <div class="fundo2"><img src="img/logo-fatec_itapira.png" alt="logo" class="logo-fatec"></div>
+    </div>
+        <nav>
+            <a href="index-professor.html" class="botao-nav">Início</a>
+
+            <!-- Dropdown para 'Enviar formulário' -->
+            <div class="dropdown">
+                <a href="#" class="botao-nav dropdown-btn">Enviar formulário</a>
+                <div class="dropdown-content">
+                    <a href="form-justificativa.php" class="botao-nav">Justificativa de falta</a>
+                    <a href="form-reposicao.php" class="botao-nav">Reposição de aulas</a>
+                </div>
+            </div>
+
+            <a href="lista-enviados.html" class="botao-nav">Formulários enviados</a>
+            <a href="login.html" class="botao-nav">Sair</a>
+        </nav>
+    </div>
+</header>
