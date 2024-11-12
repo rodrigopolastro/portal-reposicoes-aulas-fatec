15a6a907c91c60394a2d768e3710a55bfdd6262c
Rodrigo Polastro <rodrigo.polastro03@gmail.com>
2024-11-11 21:24:28 -0300

    fix: corrige sql da consulta de disciplinas de uma justificativa

--- models/disciplinas.php
+++ models/disciplinas.php
@@ -6,7 +6,9 @@ function selectDisciplinaProfessorHorario($idProfessor, $idHorario)
     global $conexao;
     $sql = $conexao->prepare(
         "SELECT DISTINCT
+            CUR_id,
             CUR_sigla,
+            DCP_id,
             DCP_sigla,
             DCP_nome,
             DCP_semestre
@@ -30,14 +32,21 @@ function selectDisciplinasJustificativa($idJustificativa)
     global $conexao;
     $sql = $conexao->prepare(
         "SELECT DISTINCT
+            CUR_id,
             CUR_sigla,
+            DCP_id,
             DCP_sigla,
             DCP_nome,
             DCP_semestre
         FROM DISCIPLINAS
         INNER JOIN CURSOS ON CUR_id = DCP_id_curso
-        INNER JOIN JUSTIFICATIVAS_FALTAS ON JUF_id_professor = DCP_id_professor
-        WHERE JUF_id = :id_justificativa"
+        INNER JOIN HORARIOS_DISCIPLINAS ON HRD_id_disciplina = DCP_id
+        INNER JOIN HORARIOS_FATEC ON HRF_id = HRD_id_horario
+        INNER JOIN HORARIOS_AUSENCIAS ON HRA_id_horario = HRF_id
+        INNER JOIN JUSTIFICATIVAS_FALTAS ON JUF_ID = HRA_id_justificativa
+        WHERE DCP_id_professor = JUF_id_professor
+        AND JUF_id = :id_justificativa
+        ORDER BY HRA_data_falta, HRF_horario_inicio"
     );
 
     $sql->bindValue('id_justificativa', $idJustificativa);
